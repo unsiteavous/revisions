6fbcc3be96e78ec2a50e71ad12c4911d446ed82c
Théophile Bellintani <theophile@captp.fr>
2024-09-11 15:07:33 +0200

    ajout du File Upload en php natif

---
+++ 04 file upload/php natif/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>téléverser une image</title>
+</head>
+<body>
+  <form action="traitement.php" method="post">
+    <input type="file" name="image"><br>
+    <input type="submit" value="Téléverser"><br>
+  </form>
+</body>
+</html>
